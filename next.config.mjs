6c5cc88e640adc74ea1cd8ecc3c3d24edc3f6304
HuzaifaRahi1 <harain661@gmail.com>
2025-01-21 19:07:41 +0500

    handled errors o.1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['cdn.sanity.io'],  // Allow Sanity CDN images
+  },
+};
 
 export default nextConfig;
